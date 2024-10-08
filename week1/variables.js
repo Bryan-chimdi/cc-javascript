89a4287e8271f70f41eba8229b45acb0f225257c
Bryan-chimdi <Chimdibryan8@gmail.com>
2024-10-08 14:21:38 -0700

    learn about variables and operators

---
+++ week1/variables.js
@@ -0,0 +1,57 @@
+// alert('hello')\
+// gjhvv bjy
+// y yvb yvhbbiryv
+// zby hbuvb
+// avrtvhv
+
+let fname= 'bryan';
+// alert(fname);
+fname='first place';
+// alert(fname)
+
+const surname='adebayo'
+// alert(surname)
+// surname='musa';
+// alert(surname)
+
+
+let firstNum =5;
+let secondNum=2;
+let add= firstNum + secondNum;
+console.log('5 + 2 ='+add);
+let subtract = firstNum - secondNum;
+let divide = firstNum / secondNum;
+let multiply = firstNum * secondNum;
+console.log('5-2 =' + subtract);
+console.log('5/2 =' + divide);
+console.log('5*2 =' + multiply);
+let power= firstNum ** secondNum
+console.log('5 raised to power 2= ' + power)
+let remainder = firstNum % secondNum;
+console.log('5 divided by 2 is '+ divide + remainder)
+console.log('5 divided by 2 is 2 r ' + remainder)
+console.log('5>2= ' + (firstNum > secondNum))
+console.log('5<2= ' + (firstNum < secondNum))
+console.log('5>=2= ' + (firstNum >= secondNum))
+console.log('5<=2= ' + (firstNum <= secondNum))
+console.log('5 is equal to 5 ' + (firstNum == firstNum))
+console.log('5' == 5)
+console.log('5' === 5)
+console.log('5 is not equal to 2 ' + (firstNum != secondNum))
+
+let coloroftop ='white';
+let coloroflegs= 'ash';
+let numberoflegs= 4;
+let length= 1.5;
+let isfoldable= true;
+let materialoftop= 'plastic';
+let materialoflegs= 'aluminium';
+let isheavy= false;
+let texture='coarse';
+let nooftables=6;
+
+
+// using objects in your environment, create 30 variables of the the major data types
+// 10 numbers
+// 10 strings
+// 10 boolean values
